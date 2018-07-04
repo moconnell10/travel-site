af7e6583c5ff5625926e825212ab78dccaa6a0eb
Mari <moconnell10@hotmail.com>
2018-07-04 19:42:48 -0400

    Completed lazy loading of images.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,10 +18,10 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
-  
-  watch('./app/assets/scripts/**/*.js', function(){
-	 gulp.start('scriptsRefresh'); 
-  });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 
 });
 
@@ -30,6 +30,6 @@ gulp.task('cssInject', ['styles'], function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('scriptsRefresh', ['scripts'], function(){
-	browserSync.reload();
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
